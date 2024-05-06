ac6d6279b7ac1fe21358d028f0ab96ec1d0b12f0
Hirdesh Khandelwal <hirdeshkhandelwal58@gmail.com>
2024-05-06 21:29:09 +0530

    feat: added main route

--- src/app.js
+++ src/app.js
@@ -16,6 +16,11 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 
+// Main route
+app.get("/", (req, res) => {
+    res.send("Welcome to Stuneckt")
+})
+
 // Importing Routes 
 import userRoute from "./routes/user.route.js";
 import postRoute from "./routes/post.route.js"
